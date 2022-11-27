429d0f227fa6ecba1df589c8ff0bbb7df00bde61
dhiraj540 <dhirajsubudhi540@gmail.com>
2022-11-27 19:36:38 +0530

    color

--- src/components/Buttons/PrimaryButton.jsx
+++ src/components/Buttons/PrimaryButton.jsx
@@ -6,11 +6,11 @@ const PrimaryButton = ({ label, isSelected, border }) => {
     <div
       className={`${
         isSelected
-          ? "primary_gradient text-white font-[700]"
+          ? "primary_gradient text-white font-[700] custom_shadow"
           : "gray_gradient text-black font-[500]"
       }  px-2 py-1 rounded-full min-w-[70px] ${
         border ? "border-[1px]" : ""
-      } cursor-pointer custom_shadow  text-[13px] min-h-[35px] flex items-center justify-center`}
+      } cursor-pointer   text-[13px] min-h-[35px] flex items-center justify-center`}
     >
       {label}
     </div>
